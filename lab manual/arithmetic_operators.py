be1113c529d3f9dd1940353374c9a5d0c1fef61c
20wh1a6649 <93580048+20wh1a6649@users.noreply.github.com>
2021-11-30 12:04:32 +0530

    Create arithmetic_operators.py

---
+++ lab manual/arithmetic_operators.py
@@ -0,0 +1,24 @@
+num1 = int(input("enter num1 : "))
+num2 = int(input("Enter num2 : "))
+print(f'{num1} + {num2} = {num1 + num2}')
+print(f'{num1} - {num2} = {num1 - num2}')
+print(f'{num1} * {num2} = {num1 * num2}')
+print(f'{num1} / {num2} = {num1 / num2}')
+print(f'{num1} // {num2} = {num1 // num2}')
+print(f"{num1} % {num2} = {num1 % num2}")
+
+
+'''
+INPUT
+enter num1 : 
+4
+Enter num2 : 
+5
+OUTPUT
+4 + 5 = 9
+4 - 5 = -1
+4 * 5 = 20
+4 / 5 = 0.8
+4 // 5 = 0
+4 % 5 = 4
+'''
