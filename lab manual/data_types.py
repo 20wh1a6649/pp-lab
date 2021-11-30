b5f0278d935d021b773fcf23827413fa36514523
20wh1a6649 <93580048+20wh1a6649@users.noreply.github.com>
2021-11-30 12:01:46 +0530

    Create data_types.py

---
+++ lab manual/data_types.py
@@ -0,0 +1,17 @@
+num1 = 5
+num2 = 9.0
+num3 = 5.6
+num4 = 1 + 2j
+print(num1,"is of type","-", type(num1))
+print(num2,"is of type", "-", type(num2))
+print(num3,"is of type", "-", type(num3))
+print(num4,"is of type", "-", type(num4))
+
+'''OUTPUT
+
+   
+5 is of type - <class 'int'>
+9.0 is of type - <class 'float'>
+5.6 is of type - <class 'float'>
+(1+2j) is of type - <class 'complex'>
+'''
